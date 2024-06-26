8ae4a196ce706bf692dbdcd0a916d75cbaabb005
Priyanka-9359 <priyankachorat800@gmail.com>
2024-06-26 09:03:36 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,27 @@
+"use strict"; //treat all js code as newer version/js
+
+//alert(3 + 3) we are using node.js not browser//
+
+console.log(3 + 
+    +3) // code redeability should be high
+
+    console.log("Priyanka")
+
+    let name = "Priyanka" //string datatype
+    let age = 18 //number datatype
+    let isLoggedIn = true //boolean datatype
+
+    // primitive data types//
+    //number => 2 to power 53
+    //bigint not using more but if there are long number like in trading stock then it will used
+    //string => '' , "" "" are more usable
+    //boolean => true or false
+    //null => standalone value means empty
+    //undefined => value is not defined yet more usable
+    //symbol => to find uniqueness and it is will usable when will learn react
+
+
+    //object
+
+    console.log(typeof undefined) //output => undefined beacuse it is a type of undefined only
+    console.log(typeof undefined) //output => object
