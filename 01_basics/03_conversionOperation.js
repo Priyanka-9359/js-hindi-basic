c2633be04bbd6d2bf8df947bdc6dc2ee2d5fc80b
Priyanka-9359 <priyankachorat800@gmail.com>
2024-06-26 11:31:45 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,30 @@
+let score = "priyanka"
+
+//console.log(typeof score);
+//console.log(typeof(score)) //or we can write it as a method
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+// "33" => easily convert in 33
+// "33abc" => NaN (not an number)
+// true => 1
+// false => 0
+//null => 0
+// undefined => undefined
+
+let isLoggedIn = "priyanka"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn)
+
+// 1 => true; 0 => false
+// "" => false
+// "priyanka" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
