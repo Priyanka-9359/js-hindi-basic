76bac3bf63446611e283206782bb432cacb344d8
Priyanka-9359 <priyankachorat800@gmail.com>
2024-06-27 04:24:57 +0000

    comparison of datatypes

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,18 @@
+//console.log(2 > 1); (basic)
+//console.log(2 >= 1);
+//console.log(2 < 1);
+//console.log(2 == 1);
+//console.log(2 != 1);
+
+//console.log("2" > 1); (little hard)
+//console.log("02" > 1);
+
+//console.log(null > 0); (avoid because of confusion)
+//console.log(null == 0);
+//console.log(null >= 0);
+
+//console.log(undefined == 0); (avoid because of confusion)
+//console.log(undefined > 0);
+//console.log(undefined < 0);
+
+// === not only check its value but it strickly check datatype
